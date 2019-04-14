90434de8c0fa6793abfc333c5f5a3c62eedab67a
Michael Kishinevsky <mkishine@yahoo.com>
2019-04-14 06:55:47 -0400

    Added navbar. Made edit boxes and search button work.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import { AgGridReact } from 'ag-grid-react';
+import {AgGridReact} from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
 import rowData from './olympicWinners';
 
 const gridClassName = "ag-theme-balham";
+
+
+const changeHandler = (fn, name) => (event) => fn(name, event);
 
 class App extends Component {
     constructor(props) {
@@ -55,20 +68,61 @@ class App extends Component {
                     field: "total"
                 }
             ],
-            rowData: rowData
+            rowData: rowData,
+            portfolio: 'ISHT7-10',
+            date: '3-Jan-2019'
         };
+        this.onFormControlChange = this.onFormControlChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onFormControlChange(id, event) {
+        console.log(id + " " + event.target.value);
+        this.setState({[id]: event.target.value})
+    }
+
+    onSubmit(event) {
+        const {portfolio, date} = this.state;
+        console.log(portfolio + " " + date);
+        event.preventDefault();
     }
 
     render() {
         return (
-            <div style={{ width: "100%", height: "100%" }}>
-                <div
-                    className={gridClassName}
-                    style={{
-                        height: '100%',
-                        width: '100%'
-                    }}
-                >
+            <div style={{width: "100%", height: "100%"}}>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        <Form inline onSubmit={this.onSubmit}>
+                            <FormControl type="text"
+                                         placeholder="Portfolio"
+                                         className="mr-sm-2"
+                                         value={this.state.portfolio}
+                                         onChange={changeHandler(this.onFormControlChange, 'portfolio')}
+                            />
+                            <FormControl type="text"
+                                         placeholder="Date"
+                                         className="mr-sm-2"
+                                         value={this.state.date}
+                                         onChange={changeHandler(this.onFormControlChange, 'date')}
+                            />
+                            <Button variant="outline-success" type="submit">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Navbar>
+                <div className={gridClassName} style={{height: '100%', width: '100%'}}>
                     <AgGridReact
                         columnDefs={this.state.columnDefs}
                         rowData={this.state.rowData}>
